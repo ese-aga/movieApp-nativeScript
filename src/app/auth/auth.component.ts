a951d5e0a5faa58e8da032c25a4c388c5d634ddd
Ese Agadagba <eseogheneagadagba1@gmail.com>
2021-08-10 21:23:13 +0200

    latest changes for mobile app

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Auth } from "../models/Auth";
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import { getString, setString } from "tns-core-modules/application-settings"
+import { SnackBar } from 'nativescript-material-snackbar';
+
+@Component({
+  selector: 'ns-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+  moduleId: module.id,
+})
+export class AuthComponent implements OnInit {
+
+  public registerMode: Boolean;
+  public auth: Auth;
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.registerMode = false;
+    const myToken = getString("mr-token");
+    if(myToken){
+      this.router.navigate(['/items'])
+    } else {
+      this.auth = {username: "", password: ""};
+    }
+    
+  }
+  submitForm(){
+    if(this.registerMode){
+      this.apiService.registerUser(this.auth).subscribe(
+        (results: any) => {
+          this.loginFunction();
+        },
+        err => {
+          const snackbar = new SnackBar();
+          snackbar.simple(`cannot register, please try again`);
+        }
+      )
+    } else {
+      this.loginFunction();  
+    }
+  }
+  loginFunction(){
+    this.apiService.loginUser(this.auth).subscribe(
+      (results: any) => {
+        setString("mr-token", results.token);
+        this.router.navigate(['/items'])
+      },
+      err => {
+        console.log(err)
+        const snackbar = new SnackBar();
+        snackbar.simple(err.error.non_field_errors[0]);
+      }
+    )
+  }
+}
